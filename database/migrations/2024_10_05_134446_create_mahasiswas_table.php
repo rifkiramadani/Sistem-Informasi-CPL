344e2c3da9a174895996484c488c2cf511d6714d
rifkiramadani <rifkiramadani724@gmail.com>
2024-10-05 21:05:23 +0700

    Membuat Migrasi Table

---
+++ database/migrations/2024_10_05_134446_create_mahasiswas_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('mahasiswas', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->string('npm')->unique();
+            $table->string('password');
+            $table->integer('semester_id');
+            $table->integer('nilai_id');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('mahasiswas');
+    }
+};
