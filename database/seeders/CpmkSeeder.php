77c9f56c6c873630c5dd9f4ddbf13cc1e86d34da
rifkiramadani <rifkiramadani724@gmail.com>
2024-12-04 23:31:23 +0700

    feat: update rumusans migration and rumusan view index

--- database/seeders/CpmkSeeder.php
+++ database/seeders/CpmkSeeder.php
@@ -14,7 +14,6 @@ class CpmkSeeder extends Seeder
             'id' => 1,
             'name' => 'CPMK-01',
             'deskripsi' => 'ini adalah cpmk 01',
-            'skor_maks' => 87.3,
             'created_at' => Carbon::create('2024', '10', '14', '08', '01', '53'),
             'updated_at' => Carbon::create('2024', '10', '14', '08', '02', '01'),
         ]);
@@ -23,7 +22,6 @@ class CpmkSeeder extends Seeder
             'id' => 2,
             'name' => 'CPMK-02',
             'deskripsi' => 'ini adalah cpmk 02',
-            'skor_maks' => 78.5,
             'created_at' => Carbon::create('2024', '10', '14', '08', '02', '19'),
             'updated_at' => Carbon::create('2024', '10', '14', '08', '02', '19'),
         ]);
@@ -32,7 +30,6 @@ class CpmkSeeder extends Seeder
             'id' => 3,
             'name' => 'CPMK-03',
             'deskripsi' => 'ini adalah cpmk 03 masbrooo',
-            'skor_maks' => 87.9,
             'created_at' => Carbon::create('2024', '10', '14', '08', '02', '33'),
             'updated_at' => Carbon::create('2024', '10', '14', '18', '20', '17'),
         ]);
